893de937ba0d3987b90b5b6925049f08b1bf26b0
misa vojtova <misacastella@gmail.com>
2022-01-19 18:10:08 +0000

    cart structure

--- front-end/src/pages/MyCartPage/index.js
+++ front-end/src/pages/MyCartPage/index.js
@@ -1,4 +1,79 @@
+import React, { useState } from "react";
+import CartProduct from "../../components/CartProduct";
+import { FaCcVisa, FaCcMastercard, FaCcPaypal } from "react-icons/fa";
+import paypal from "../../components/images/paypal.png";
+import "./style.css";
+
 function MyCartPage() {
-  return <div>My cart</div>;
+  const [active, setActive] = useState(false);
+  console.log(active);
+  return (
+    <>
+      <header className='header'> HEADER</header>
+      <div className='layout-wrap'>CONTINUE SHOPPING</div>
+      <div className='main-side-wrapper'>
+        <main className='cart-content-wrap'>
+          {<CartProduct />}
+          {<CartProduct />}
+          {<CartProduct />}
+          {<CartProduct />}
+          {<CartProduct />}
+          {<CartProduct />}
+          {<CartProduct />}
+          {<CartProduct />}
+          {<CartProduct />}
+          {<CartProduct />}
+        </main>
+        <sidebar className='pay-checkout'>
+          <div className='center-pay'>
+            <h2 className='summary'>Summary</h2>
+
+            <div className='total-con'>
+              <h4>Items: </h4>
+              <h4 className='total-price'>$257.41</h4>
+            </div>
+
+            <h5 className='add-promo' onClick={() => setActive(!active)}>
+              Shipping cost {">"}
+            </h5>
+
+            {active && (
+              <input
+                type='text'
+                className='zip-code'
+                placeholder='Input your zip code'
+              />
+            )}
+            <h5 className='add-promo' onClick={() => setActive(!active)}>
+              Add promo code {">"}
+            </h5>
+            {active && (
+              <input
+                type='text'
+                className='promo-code'
+                placeholder='Input your promo code'
+              />
+            )}
+
+            <div className='final-con'>
+              <h4 className='final-cost'> Total cost: </h4>
+              <h4 className='final-price'>$257.41</h4>
+            </div>
+
+            <button className='delivery-btn'>CHECKOUT</button>
+            {/* <img className='paypal' src={paypal} alt='paypal' /> */}
+          </div>
+          <div className='secure-payment-con'>
+            <h4>Secure payment</h4>
+            <h3 className='secure-icons'>
+              <FaCcVisa /> <FaCcMastercard /> <FaCcPaypal />
+            </h3>
+          </div>
+        </sidebar>
+      </div>
+      <footer className='footer'>NEWSLETTERS ADVERTISMENTS</footer>
+      <footer className='footer'>FOOOTER</footer>
+    </>
+  );
 }
 export default MyCartPage;
