7f3aa34b146afb6cafbbd417d7f3cf0946662875
SusanaAlvarezZuluaga <susialvarezzuluaga@hotmail.com>
2022-01-21 10:32:58 +0000

    styling home-page

--- front-end/src/components/Singlecard/singleCard.js
+++ front-end/src/components/Singlecard/singleCard.js
@@ -1,18 +1,22 @@
-import React from "react";
-import "./style.css"
+import React from 'react';
+import './style.css';
 
 const SingleCard = (props) => {
   const { id, title, src, description, setCompare } = props;
   const handleOnClick = (id) => setCompare((prev) => [...prev, props]);
 
   return (
-    <div className='card-grid'>
-      <div className='card-body'>
-        <img src={src} className='card__image' alt='' />
-        <h2 className='card-title'>{title}</h2>
+    <div className="card">
+      <div className="card-body">
+        <div className="card-img-holder">
+          <img src={src} className="card-image" alt="" />
+        </div>
+        <h2 className="card-title">{title}</h2>
       </div>
-      <p className='card-description'>{description}</p>
-      <button className="btn" onClick={() => handleOnClick(id)}>Compare</button>
+      <p className="card-description">{description}</p>
+      <button className="btn" onClick={() => handleOnClick(id)}>
+        Compare
+      </button>
     </div>
   );
 };
--- front-end/src/pages/HomePage/index.js
+++ front-end/src/pages/HomePage/index.js
@@ -1,53 +1,61 @@
-import SingleCard from "../../components/Singlecard/singleCard";
-import image1 from "../../assets/Tap1.png";
-import image2 from "../../assets/Tap2.png";
-import image3 from "../../assets/Tap3.png";
-import image4 from "../../assets/Tap4.png";
-import image5 from "../../assets/Tap5.png";
-import specs1 from "../../assets/specs1.png";
-import specs2 from "../../assets/specs2.png";
-import specs3 from "../../assets/specs3.png";
-import specs4 from "../../assets/specs4.png";
-import specs5 from "../../assets/specs5.png";
-import "./style.css";
-import { Link } from "react-router-dom";
-import React from "react";
-import Navbar from "../../components/CartSlider/index";
+import SingleCard from '../../components/Singlecard/singleCard';
+import image1 from '../../assets/Tap1.png';
+import image2 from '../../assets/Tap2.png';
+import image3 from '../../assets/Tap3.png';
+import image4 from '../../assets/Tap4.png';
+import image5 from '../../assets/Tap5.png';
+import image6 from '../../assets/Tap6.png';
+import specs1 from '../../assets/specs1.png';
+import specs2 from '../../assets/specs2.png';
+import specs3 from '../../assets/specs3.png';
+import specs4 from '../../assets/specs4.png';
+import specs5 from '../../assets/specs5.png';
+import './style.css';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import Navbar from '../../components/CartSlider/index';
 
 const products = [
   {
     id: 1,
     src: image1,
-    title: "Sauber Kitchen Mixer Tap Dual Spout with Pull Out Spray",
-    description: "",
+    title: 'Sauber Kitchen Mixer Tap Dual Spout with Pull Out Spray',
+    description: '',
     specs: specs1,
   },
   {
     id: 2,
     src: image2,
-    title: "Sauber Baden Pull Out Kitchen Mixer Tap",
-    description: "",
+    title: 'Sauber Baden Pull Out Kitchen Mixer Tap',
+    description: '',
     specs: specs2,
   },
   {
     id: 3,
     src: image3,
-    title: "Sauber Kitchen Mixer Tap with Pull Out Spray",
-    description: "",
+    title: 'Sauber Kitchen Mixer Tap with Pull Out Spray',
+    description: '',
     specs: specs3,
   },
   {
     id: 4,
     src: image4,
-    title: "Sauber Aarau Kitchen Mixer Tap Chrome",
-    description: "",
+    title: 'Sauber Aarau Kitchen Mixer Tap Chrome',
+    description: '',
     specs: specs4,
   },
   {
     id: 5,
     src: image5,
-    title: "Sauber Vevey Brushed Kitchen Mixer Tap",
-    description: "",
+    title: 'Sauber Vevey Brushed Kitchen Mixer Tap',
+    description: '',
+    specs: specs5,
+  },
+  {
+    id: 6,
+    src: image6,
+    title: 'Sauber Baden Pull Out Kitchen Mixer Tap - Brushed',
+    description: '',
     specs: specs5,
   },
 ];
@@ -55,18 +63,21 @@ const products = [
 function HomePage({ compare, setCompare }) {
   return (
     <>
-      <header className='header'>
-        HEADER
+      <header className="header">
         <Navbar />
       </header>
-      <div className='layout-wrap'>CONTINUE SHOPPING</div>
-      <div className='wrapper'>
-        <Link to='/compare'>
-          Compare {compare.length > 0 ? compare.length : null}
-        </Link>
-        {products.map((product) => (
-          <SingleCard setCompare={setCompare} key={product.id} {...product} />
-        ))}
+      <div className="page-content-holder">
+        <div className="compare-button">
+          <Link to="/compare">
+            Compare {compare.length > 0 ? compare.length : null}
+          </Link>
+        </div>
+
+        <div className="product-cards-holder">
+          {products.map((product) => (
+            <SingleCard setCompare={setCompare} key={product.id} {...product} />
+          ))}
+        </div>
       </div>
     </>
   );
